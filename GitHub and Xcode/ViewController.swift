1c10b184dd1e4284008303a81ef65d195b68c457
Ryan Mai <rmai21@andover.edu>
2019-12-17 12:09:13 -0500

    Initial Commit

---
+++ GitHub and Xcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHub and Xcode
+//
+//  Created by Ryan Mai on 12/17/19.
+//  Copyright © 2019 Ryan Mai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
